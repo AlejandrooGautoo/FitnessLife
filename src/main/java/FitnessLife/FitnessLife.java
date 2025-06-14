be750e649cc1799de1e7290ef2909208cf5b2eda
xxalegautoxx <xxalegautoxx@gmail.com>
2025-06-13 22:26:42 -0300

    Initial commit

---
+++ src/main/java/FitnessLife/FitnessLife.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package FitnessLife;
+
+/**
+ *
+ * @author ALE
+ */
+public class FitnessLife {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
